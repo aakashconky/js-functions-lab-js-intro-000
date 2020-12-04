1803740c02f7c29cd79d85b2b9114ddb8fc6bbc6
aakash thapa <aakash_conky@hotmail.com>
2020-12-04 06:48:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function happyCustomHolidayTo (holiday , name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown 
+function holidayCountdown (holiday, days) {
+  return `It\`s ${days} days until ${holiday}!`
+}
 
